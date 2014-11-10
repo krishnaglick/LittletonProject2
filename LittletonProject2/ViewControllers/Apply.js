0d98bba48aa8add7714e7814e45ef199230ea2a9
Krishna Glick <unstoppable411@gmail.com>
2014-11-09 19:28:51 -0500

    Modal data is being saved, and validation is being checked, but modal is closing when no save happens :(

--- LittletonProject2/ViewControllers/Apply.js
+++ LittletonProject2/ViewControllers/Apply.js
@@ -1,44 +1,72 @@
 ﻿//When document is ready, run what's in this block
 $(function () {
+    //Set up the viewmodel
     var apply_view_model = new ApplyViewModel();
     ko.applyBindings(apply_view_model);
     apply_view_model.getStates();
     apply_view_model.getMilitaryBranches();
 
+    //Activate the datepicker!
     $('.datepicker').datepicker({
         inline: true,
         showOtherMonths: true,
         dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     });
 
-    $('.dates input:button').not('.selectAll').click(function () {
-        $(this).toggleClass('selected');
+    //Pick days of week you can work
+    $('.buttonDays').click(function () {
+        $(this).toggleClass('round-button-selected');
+    });
+    var allTracker = true;
+    $('.selectAll').click(function () {
+        if (allTracker) {
+            $('.buttonDays').addClass('round-button-selected');
+            allTracker = false;
+        }
+        else {
+            $('.buttonDays').removeClass('round-button-selected');
+            allTracker = true;
+        }
     });
 
-    $('.dates .selectAll').click(function () {
-        $('.dates input:button').not('.selectAll').click();
-    });
-
+    //Activate the modals!
     $('#addWorkExp').click(function () {
         $('#addWorkExpModal').modal();
     })
-
     $('#addMilitaryExp').click(function () {
         $('#addMilitaryExpModal').modal();
     })
-
     $('#addReference').click(function () {
         $('#addReferenceModal').modal();
     })
 
-    $('#addReference').click(function () {
-
+    //Button toggling cause bootstrap don't do that shit on its own
+    $('#FullTime').click(function () {
+        $('#FullTime').addClass('active');
+        $('#PartTime').removeClass('active');
+    })
+    $('#PartTime').click(function () {
+        $('#FullTime').removeClass('active');
+        $('#PartTime').addClass('active');
+    })
+    $('#NightsYes').click(function () {
+        $('#NightsYes').addClass('active');
+        $('#NightsNo').removeClass('active');
+    })
+    $('#NightsNo').click(function () {
+        $('#NightsYes').removeClass('active');
+        $('#NightsNo').addClass('active');
+    })
+    $('#FiredYes').click(function () {
+        $('#FiredYes').addClass('active');
+        $('#FiredNo').removeClass('active');
+    })
+    $('#FiredNo').click(function () {
+        $('#FiredYes').removeClass('active');
+        $('#FiredNo').addClass('active');
     })
 
-    $('#saveWorkExp').click(function () {
-
-    })
-
+    //Clear out modals when they close
     $('.modal').on('hidden.bs.modal, show.bs.modal', function () {
         $(this).find('input:text').val("");
         $(this).find('input:checkbox').attr('checked', false);
--- LittletonProject2/ViewModels/ApplyViewModel.js
+++ LittletonProject2/ViewModels/ApplyViewModel.js
@@ -1,6 +1,8 @@
 ﻿var ApplyViewModel = function () {
+    //Stuff used from pullback
     this.listOfStates = ko.observableArray([]);
     this.militaryBranches = ko.observableArray([]);
+    //Tracking
     this.employers = ko.observableArray([]);
     this.militaryExp = ko.observableArray([]);
     this.references = ko.observableArray([]);
@@ -39,15 +41,46 @@ ApplyViewModel.prototype.getMilitaryBranches = function () {
 /*  Planning to use selectors to build a new item to push into one of my obserables, for showing on a page.
     Not sure if this is the best way though.                                                             */
 
-ApplyViewModel.prototype.addEmployer = function () {
-    var self = this;
-    console.log($('#employerDuties').val());
+ApplyViewModel.prototype.addEmployer = function (event) {
+    if ($('#addWorkExpModal').find(':invalid').length > 0)
+    {
+        alert("potato");
+        event.preventDefault();
+    }
+
+    this.employers.push({
+        name: $('#employerName').val(),
+        email: $('#employerEmail').val(),
+        canContact: $('#employerCanContact').is(':checked'),
+        street: $('#employerStreet').val(),
+        city: $('#employerCity').val(),
+        state: $('#employerState').val(),
+        boss: $('#employerLastBoss').val(),
+        phone: $('#employerPhone').val(),
+        startDate: $('#employerStartDate').val(),
+        endDate: $('#employerEndDate').val(),
+        duties: $('#employerDuties').val()
+    });
 }
 
 ApplyViewModel.prototype.addMilitaryExp = function () {
-    var self = this;
+    this.militaryExp.push({
+        years: $('#yearsInMilitary').val(),
+        years: $('#militaryBranch').val(),
+        reserve: $('#inReserve').is(':checked'),
+        discharge: $('#honorableDischarge').is(':checked')
+    });
 }
 
 ApplyViewModel.prototype.addReference = function () {
-    var self = this;
+    this.references.push({
+        name: $('#referenceName').val(),
+        title: $('#yearsInMilitary').val(),
+        company: $('#referenceTitle').val(),
+        street: $('#referenceStreetAddress').val(),
+        city: $('#referenceCity').val(),
+        state: $('#referenceState').val(),
+        phone: $('#referencePhone').val(),
+        email: $('#referenceEmail').val()
+    });
 }
