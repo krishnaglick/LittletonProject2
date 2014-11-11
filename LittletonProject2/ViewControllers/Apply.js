5ce5e5359e98aeecadbbe34ba3f818ec6c7b9446
mrutherford2 <mattrutherford@marionsoftware.com>
2014-11-11 16:17:05 -0500

    Validation is working, modals are now forms

--- LittletonProject2/ViewControllers/Apply.js
+++ LittletonProject2/ViewControllers/Apply.js
@@ -105,13 +105,40 @@ function checkDates(value, comparedTo){
 }
 
 function applyValidation() {
-    //LiveValidation is done here
+    //LiveValidation for personal info
     new LiveValidation('firstName').add(Validate.Presence);
     new LiveValidation('lastName').add(Validate.Presence);
     new LiveValidation('City').add(Validate.Presence);
     new LiveValidation('streetAddress').add(Validate.Presence);
     new LiveValidation('mobilePhone').add(Validate.Presence).add(Validate.Format, { pattern: /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/i });
     new LiveValidation('homePhone').add(Validate.Presence).add(Validate.Format, { pattern: /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/i });
-    new LiveValidation('dateAvailableEmployment').add(Validate.Presence);
+    
+    //LiveValidation for Employment Info 
+    new LiveValidation('dateAvailableEmployment').add(Validate.Presence).add(Validate.Format, { pattern: /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/i});
 
+    //LiveValidation for AddEmployer Modal
+    new LiveValidation('employerName').add(Validate.Presence);
+    new LiveValidation('employerEmail').add(Validate.Presence).add(Validate.Format, { pattern: /^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/i });
+    new LiveValidation('employerStreet').add(Validate.Presence);
+    new LiveValidation('employerCity').add(Validate.Presence);
+    new LiveValidation('employerLastBoss').add(Validate.Presence);
+    new LiveValidation('employerPhone').add(Validate.Presence).add(Validate.Format, { pattern: /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/i }); 
+    new LiveValidation('employerStartDate').add(Validate.Presence).add(Validate.Format, { pattern: /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/i });
+    new LiveValidation('employerEndDate').add(Validate.Presence).add(Validate.Format, { pattern: /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/i });
+    new LiveValidation('employerDuties').add(Validate.Presence);
+
+    //LiveValidation for Reference Modal
+    new LiveValidation('referenceName').add(Validate.Presence);
+    new LiveValidation('referenceTitle').add(Validate.Presence);
+    new LiveValidation('referenceCompany').add(Validate.Presence);
+    new LiveValidation('referenceStreetAddress').add(Validate.Presence);
+    new LiveValidation('referenceCity').add(Validate.Presence);
+    new LiveValidation('referencePhone').add(Validate.Presence).add(Validate.Format, { pattern: /^\D?(\d{3})\D?\D?(\d{3})\D?(\d{4})$/i });
+    new LiveValidation('referenceEmail').add(Validate.Presence).add(Validate.Format, { pattern: /^[-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}/i });
+
+    //LiveValidation for Education Modal
+    new LiveValidation('schoolName').add(Validate.Presence);
+    new LiveValidation('schoolCity').add(Validate.Presence);
+    new LiveValidation('gradDate').add(Validate.Presence).add(Validate.Format, { pattern: /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/i });
+    new LiveValidation('majorDegCert').add(Validate.Presence); 
 }
